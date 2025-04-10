563c1cf43c7160c568753c823b7bd0fcc6eb58f0
august-unsainted <mhidt_unsainted@mail.ru>
2025-04-11 00:34:42 +0800

    — Добавлен второй ключ AI_KEY для увеличения количества запросов — В начале поста теперь есть дата и время публикации — Отправка ошибок в чат для мониторинга работы

--- utils/ai.py
+++ utils/ai.py
@@ -1,15 +1,21 @@
-from openai import OpenAI
-from config import AI_KEY, AI_MODEL, AI_PROMPT
+from openai import AsyncOpenAI
+from config import AI_KEY1, AI_KEY2, AI_MODEL, AI_PROMPT
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=AI_KEY,
-)
+keys = [AI_KEY1, AI_KEY2]
 
 
-async def send_ai_request(text: str) -> str:
-    completion = client.chat.completions.create(
-        model=AI_MODEL,
-        messages=[{"role": "user",
-                   "content": AI_PROMPT.format(text)}])
-    return completion.choices[0].message.content
+async def send_ai_request(text: str) -> str | None:
+    for current_key in keys:
+        client = AsyncOpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=current_key,
+        )
+        for _ in range(2):
+            completion = await client.chat.completions.create(
+                model=AI_MODEL,
+                messages=[{"role": "user", "content": AI_PROMPT.format(text)}]
+            )
+            answer = completion.choices[0].message.content
+            if answer is not None:
+                return answer
+    return None
